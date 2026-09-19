1323add058b17d097d682d6d3f6c0c958ecf4767
MooZeKMZK <mooz.05@hotmail.com>
2026-09-19 19:56:23 +0200

    refactor: reorganize scripts and update scene

---
+++ Assets/scripts/UI.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [Header("UI References")] 
+    [SerializeField]public SerializableDictionary<ItemType, Image> inventorySlots;
+    public TextMeshProUGUI changeText;
+
+    private void OnUpdateItemPickup(bool enable)
+    {
+        changeText.gameObject.SetActive(enable);        
+    }
+
+    private void Awake()
+    {
+        OnUpdateItemUI(null);
+    }
+
+    // Funkcja aktualizująca wygląd (kolory i rozmiar)
+    private void OnUpdateItemUI([CanBeNull] PlayerInfo playerInfo)
+    {   
+        var inventory = playerInfo?.inventory ?? new SerializableDictionary<ItemType, ItemTags>
+        {
+            [ItemType.Primary] = null,
+            [ItemType.Secondary] = null,
+            [ItemType.Melee] = null,
+            [ItemType.Utils] = null
+        };;
+        var currentItem = playerInfo?.currentItemType ?? ItemType.Primary;
+        
+        
+        foreach (var inventorySlot in inventorySlots)
+        {
+            if (inventorySlot.Key.Equals(currentItem))
+            {
+                inventorySlot.Value.color = inventory.ContainsKey(currentItem) ? Color.yellow : new Color(1f, 0.92f, 0.016f, 0.5f);
+                inventorySlot.Value.transform.localScale = new Vector3(1.15f, 1.15f, 1f); // Lekko powiększony
+            }
+            else
+            {
+                inventorySlot.Value.color = inventory.ContainsKey(inventorySlot.Key) ? Color.white : new Color(1f, 1f, 1f, 0.2f);
+                inventorySlot.Value.transform.localScale = Vector3.one; // Normalny rozmiar
+            }
+            
+        }
+    }
+}
